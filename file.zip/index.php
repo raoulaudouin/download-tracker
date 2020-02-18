54379bceaad577f28fca773396d918cb77749b50
Raoul Audouin <raudouin@gmail.com>
2020-02-18 15:57:24 +0100

    added log clearing function for new files

--- file.zip/index.php
+++ file.zip/index.php
@@ -1,9 +1,22 @@
 <?
 
-$downloadFile = basename(__DIR__);
-
 $logFile = 'log.txt';
 
+if (file_exists($logFile)) {
+	// Checks the first line of the log file
+	$logFirstLine = fgets(fopen($logFile, 'r'));
+} else {
+	die('The log file does not exist.');
+}
+
+$downloadFile = basename(__DIR__);
+
+if ($logFirstLine !== $downloadFile.PHP_EOL) {
+	// if the first line is not $downloadFile, clear the log
+	file_put_contents($logFile, $downloadFile.PHP_EOL);
+}
+
+// stores download time and client IP in the log
 $isoRequestTime = date("r", $_SERVER['REQUEST_TIME']);
 $clientIp = $_SERVER['REMOTE_ADDR'];
 $downloadInfo = $isoRequestTime."\t".$clientIp.PHP_EOL;
